e064fb7060ab2bbf6ec927dc315931e990d5db21
Michael Shull <shullmb@gmail.com>
2019-02-04 16:12:14 -0800

    gathering assets from old portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/mbshull_logo.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+          <img src={logo} alt="mbshull.com" />
+          <h1>Michael Shull | Full Stack Web Development</h1>
         </header>
+        <main>
+          <p>
+            main content goes here
+          </p>
+        </main>
       </div>
     );
   }
