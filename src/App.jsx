44ace98ef4e12bd72e275c0cb5682dd3905996f0
Michael Shull <shullmb@gmail.com>
2019-02-04 16:28:37 -0800

    adding basic layout style

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,25 @@ import logo from './img/mbshull_logo.png';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div id='App'>
         <header>
-          <img src={logo} alt="mbshull.com" />
           <h1>Michael Shull | Full Stack Web Development</h1>
         </header>
+        <nav>
+          <img src={logo} alt="mbshull.com" />
+          <ul>
+            <li>nav item 1</li>
+            <li>nav item 2</li>
+            <li>nav item 3</li>
+            <li>nav item 4</li>
+          </ul>
+        </nav>
         <main>
           <p>
             main content goes here
           </p>
         </main>
+        <footer> this is the footer</footer>
       </div>
     );
   }
